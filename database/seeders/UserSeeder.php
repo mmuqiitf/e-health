e1f2e7222d58e8c2f7d85014fc71b68fc0116900
M. Muqiit Faturrahman <mmuqiit.f14@gmail.com>
2024-05-15 21:18:54 +0700

    init authentication & roles

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
+
+        $admin->assignRole('Admin');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@mail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
+
+        $user->assignRole('User');
+
+        User::factory(20)->create()->each(function ($user) {
+            $user->assignRole('User');
+        });
+    }
+}
